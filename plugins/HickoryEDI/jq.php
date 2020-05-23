f7b343f1b2f887b14e8098ffd780cc3d55bad8d5
Michael J. Sabal <mike@msabal.net>
2020-05-23 10:22:55 -0400

    Proprietary plugin module should not have been added to open source.

--- plugins/HickoryEDI/jq.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$baseurl = (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'];
-	echo '<SCRIPT type="text/javascript">function openHickoryEDI(mode) {var url="'.$baseurl.'/edi"; if (mode=="admin") url += "/admin"; var win=window.open(url,"_blank"); win.focus();}</SCRIPT>';
-	echo "<DIV id=\"HickoryEDIProcessModuleIcon\" class=\"DashboardIcon\" onClick=\"openHickoryEDI();\">Process</DIV>\r\n";
-	echo "<DIV id=\"HickoryEDIAdminModuleIcon\" class=\"DashboardIcon\" onClick=\"openHickoryEDI('admin');\">Admin</DIV>\r\n";
-?>
